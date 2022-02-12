561fdc259d9f892fffde896f0243842d790a9bac
utalmighty <utalmighty@gmail.com>
2022-02-12 22:59:37 +0530

    understanding JS functions

---
+++ functions.js
@@ -0,0 +1,44 @@
+
+// function are treated as first class citizen in JS
+// function can be returned // yes you heard it right
+// function can be passed as as arguments.
+
+//Eg:
+function outer(a, b){
+    return function inner(){
+        console.log(a+b);
+    }
+}
+
+let returnedFunction = outer(5,3);
+let returnedFunction2 = outer(15,5);
+
+console.log("Function calls made");
+
+console.log(returnedFunction); // here we have got the functions
+console.log(returnedFunction2); // here we have got the functions
+// returned object is function  
+// try to understand the output.
+
+returnedFunction(5, 5); // 8
+returnedFunction2(); // 20
+returnedFunction(); // 8
+
+// note that the function which is returned is not computed while returning but computed when called
+// Eg in output you will see   
+// Function calls made
+// 8
+// 20
+// 8
+// note 8,20,8 is printed after the outer function call is made
+
+
+
+let sayHi = function foo(name){ // this is called function Expression, therefore the foo name itself can be empty ie function ()
+    console.log("HELLO", name);
+} 
+// this sayHi varible stores function object
+// to call this function we need to call sayHi(), foo() will not be accessible
+//foo(); // Error: not defined
+sayHi("utkarsh") //HELLO utkarsh
+sayHi() // HELLO undefined
