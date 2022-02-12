f78ad823346f1a080e83bba4b66e1deba8d55338
utalmighty <utalmighty@gmail.com>
2022-02-12 23:53:11 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+// Collection of elements
+let x = ["a", true, 1, 1.12];
+x.push(12); // add at last
+x.pop(); // remove from last
+x.unshift("beggin"); // add in front
+x.shift(); // remove from front
+console.table(x); // helps in printing in table
+
+let d3 = [[[1,2,3], [4,5,6]], [[7,8,9], [10, 11, 12]]];
+console.table(d3);
